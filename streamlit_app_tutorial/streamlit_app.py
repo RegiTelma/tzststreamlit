cbce93418f2826a273435039304905a8eae58b73
RegiTelma <128974517+RegiTelma@users.noreply.github.com>
2023-07-27 13:36:09 +0200

    Add files via upload

---
+++ streamlit_app_tutorial/streamlit_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# importing customer churn data
+data = pd.read_csv("./data/training_data.csv", index_col=0)
+st.write("Churn Data")
+st.write(data)
+
+st.write("How many customers in the dataset churned?")
+target_bins = data.loc[:, 'Churn'].value_counts()
+st.bar_chart(target_bins)
+
